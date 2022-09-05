5833e66df5065e4d8fcc5f044a6ed8f33178d792
ViniPierre <pierre.vinicius@acad.espm.br>
2022-09-05 14:31:50 -0300

    Atividade

---
+++ ListaExercicio02/item.py
@@ -0,0 +1,44 @@
+class Item:
+    def __init__(self, nome = '', preco = 0.0, estoque = 0):
+        self.nome = nome
+        self.preco = preco
+        self.estoque = estoque
+        
+    def __str__(self):
+        preco = str(self.preco)
+        estoque = str(self.estoque)
+        return self.nome + " x " + estoque + " --> R$ " + preco 
+    
+class Pedido:
+    def __init__(self, pagamento = ''):
+        self.pagamento = pagamento
+        self.total = 0
+
+    def adicionaritens(self, item, qtd):
+        self.total += item.preco * qtd
+        item.estoque -= qtd
+        
+    def __str__(self):
+        preco = str(self.preco)
+        
+        return self.nome + " x " + estoque + " --> R$ " + preco 
+
+
+
+
+def main():
+    objeto01 = Item('Arroz', 10.0, 30) 
+    pedido = Pedido('debito')
+    pedido.adicionaritens(objeto01, 5)
+
+    print("--------------------")
+    print("Supermecado do André")
+    print("--------------------")
+    print("")
+    print(objeto01)
+    print("")
+    print("--------------------")
+    print(pedido.total)
+
+if __name__ == '__main__':
+    main()
